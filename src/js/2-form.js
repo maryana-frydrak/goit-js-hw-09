5bab9eaf3f433cbe254329083b5fbabeb5f5113b
maryana-frydrak <frydra4ka@gmail.com>
2026-02-28 21:45:36 +0200

    make second task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,26 @@
-console.log("Form");
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+let formData = { email: "", message: "" };
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", e => {
+    formData[e.target.name] = e.target.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if (formData.email === "" || formData.message === "") {
+        return alert("Fill please all fields");
+    }
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    formData = { email: "", message: "" };
+});
