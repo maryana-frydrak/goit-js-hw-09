5680c58ecc21ea879066bfe1ac640891a73cacfe
maryana-frydrak <frydra4ka@gmail.com>
2026-02-28 19:46:04 +0000

    Deploying to gh-pages from @ maryana-frydrak/goit-js-hw-09@5bab9eaf3f433cbe254329083b5fbabeb5f5113b 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const l="feedback-form-state",t=document.querySelector(".feedback-form");let e={email:"",message:""};const s=localStorage.getItem(l);s&&(e=JSON.parse(s),t.elements.email.value=e.email,t.elements.message.value=e.message);t.addEventListener("input",a=>{e[a.target.name]=a.target.value.trim(),localStorage.setItem(l,JSON.stringify(e))});t.addEventListener("submit",a=>{if(a.preventDefault(),e.email===""||e.message==="")return alert("Fill please all fields");console.log(e),localStorage.removeItem(l),t.reset(),e={email:"",message:""}});
 //# sourceMappingURL=2-form.js.map
